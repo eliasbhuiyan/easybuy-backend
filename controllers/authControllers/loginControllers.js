d4f93c9e57ea6b893288a4cf0e2f6c45daf7308a
bhuiyan2001 <fathanbhuiyan2001@gmail.com>
2024-02-10 09:14:21 +0600

    login

---
+++ controllers/authControllers/loginControllers.js
@@ -0,0 +1,5 @@
+const loginControllers = async (req, res)=>{
+    res.send('Login')
+}
+
+module.exports = loginControllers
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const registration = require('../../controllers/authControllers/registrationControllers')
+const loginControllers = require('../../controllers/authControllers/loginControllers')
 const router = express.Router()
 
 router.post('/registration', registration)
+// will be changed with post request
+router.get('/login', loginControllers)
 
 module.exports = router

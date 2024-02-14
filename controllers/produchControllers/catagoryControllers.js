eb7d3fa4fddaf528953b8fb17bb840d9654806bf
bhuiyan2001 <fathanbhuiyan2001@gmail.com>
2024-02-14 07:26:11 +0600

    sub catagory

--- controllers/produchControllers/catagoryControllers.js
+++ controllers/produchControllers/catagoryControllers.js
@@ -1,4 +1,8 @@
 const Catagory = require('../../modal/catagorySchema.js')
+const SubCatagory = require('../../modal/subCatagory.js')
+// ================ ===================== ================
+// ================ Create Catagory Start ================
+// ================ ===================== ================
 const catagory = async (req, res) => {
     const {name, description} = req.body;
     const existingCatagory = await Catagory.find({name});
@@ -13,4 +17,44 @@ const catagory = async (req, res) => {
     catagory.save()
     res.send({message: 'Catagory created'})
 }
-module.exports = catagory
+
+// ================ ========================= ================
+// ================ Create Sub Catagory Start ================
+// ================ ========================= ================
+const subCatagory = async (req, res) => {
+    const {name, description, catagory} = req.body;
+    const existingSubCatagory = await SubCatagory.find({name});
+    if(existingSubCatagory.length > 0){
+        return res.send({error: 'Sub Catagory already in used !'})
+    }
+    
+    const subCatagory = new SubCatagory({
+        name,
+        description,
+        catagory
+    })
+    subCatagory.save()
+    await Catagory.findOneAndUpdate({_id: subCatagory.catagory}, {$push: {subCatagory: subCatagory._id}});
+    res.send({message: 'Sub Catagory created'})
+}
+
+// ================ ===================== ================
+// ================ Render Catagory Start ================
+// ================ ===================== ================
+
+const renderCatagory = async (req, res) => {
+    const catagory = await Catagory.find({}).populate('subCatagory')
+    res.send({catagory})
+}
+
+// ================ ========================= ================
+// ================ Render Sub Catagory Start ================
+// ================ ========================= ================
+const renderSubCatagory = async (req, res) => {
+    const catagory = await Catagory.find({}).populate('subCatagory')
+    res.send({catagory})
+}
+
+
+
+module.exports = {catagory,subCatagory,renderCatagory, renderSubCatagory}
--- routes/api/catagory.js
+++ routes/api/catagory.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const catagory = require('../../controllers/produchControllers/catagoryControllers')
+const {catagory, subCatagory, renderCatagory} = require('../../controllers/produchControllers/catagoryControllers')
 const router = express.Router()
 
 router.post('/createcatagory', catagory)
+router.post('/createsubcatagory', subCatagory)
+router.get('/getallcatagory', renderCatagory)
 
 
 module.exports = router

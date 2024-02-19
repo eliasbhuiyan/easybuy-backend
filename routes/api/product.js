48e93da40676b5bc05951f27698ee48d34efcb71
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-02-19 17:25:24 +0600

    multer middleware update

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require('../../utilities/multer')
+const upload = require('../../middleware/multer')
 const {
   createProduct, secureUpload, createVariant
 } = require("../../controllers/productControllers/productControllers");

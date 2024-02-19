b1980e086f3742fcc429dea972c5d36ca8f2f78e
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-02-19 17:20:08 +0600

    multer middleware

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer')
+const upload = require('../../utilities/multer')
 const {
   createProduct, secureUpload, createVariant
 } = require("../../controllers/productControllers/productControllers");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.')[1])
-  }
-})
-const upload = multer({ storage: storage })
+
 
 router.post("/createproduct", secureUpload, createProduct);
 router.post("/createvariant", secureUpload, upload.single('image'), createVariant);

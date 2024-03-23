f068529100b126011bebed997bd795dcfd3ee86e
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-03-23 16:32:44 +0600

    debug

--- controllers/authControllers/otpMatchControllers.js
+++ controllers/authControllers/otpMatchControllers.js
@@ -4,20 +4,24 @@ const otpMatch = async (req, res) => {
   if (!otp) {
     return res.status(400).send({ error: "OTP is required!" });
   } else {
-    const existingUser = await User.find({ _id: userId });
-    if (existingUser.length > 0) {
-      if (existingUser[0].otp === otp) {
-        await User.findOneAndUpdate(
-          { _id: userId },
-          { $set: { otp: null, emailVerified: true } },
-          { new: true }
-        );
-        return res.status(200).send({ message: "OTP verified!" });
+    try {
+      const existingUser = await User.find({ _id: userId });
+      if (existingUser.length > 0) {
+        if (existingUser[0].otp === otp) {
+          await User.findOneAndUpdate(
+            { _id: userId },
+            { $set: { otp: null, emailVerified: true } },
+            { new: true }
+          );
+          return res.status(200).send({ message: "OTP verified!" });
+        } else {
+          return res.status(400).send({ error: "Invalid OTP!" });
+        }
       } else {
-        return res.status(400).send({ error: "Invalid OTP!" });
+        return res.status(400).send({ error: "User not found!" });
       }
-    } else {
-      return res.status(400).send({ error: "User not found!" });
+    } catch (error) {
+      return res.status(400).send({ error: "Invalid Attempt !" });
     }
   }
 };

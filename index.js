24d1b133dd0af40d7dd8bdc228bfbfffc54f5154
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-03-24 11:15:14 +0600

    netlify

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ routes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
-  credentials: true
-}));
-
+app.use(cors(
+  {
+    origin: [
+      "https://easybuy-dashbord.netlify.app/",
+    ],
+    credentials: true
+  }
+));
 dbconfig();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(routes);

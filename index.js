b108fad00a25a76f3e53b25022f59d0231961808
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-04-06 13:38:25 +0600

    route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors(
     origin: [
       "https://easybuy-dashbord.vercel.app",
       "http://localhost:5173",
+      "https://easybuy-one.vercel.app"
     ],
     credentials: true
   }
